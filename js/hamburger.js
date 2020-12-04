f8b8dc093637bf91caee6aa8c46c15d60e3a87d8
yuto0623 <67817556+yuto0623@users.noreply.github.com>
2020-12-04 20:06:54 +0900

    first commit

---
+++ js/hamburger.js
@@ -0,0 +1,18 @@
+function hamburger() {
+  document.getElementById('hamburger-content').classList.toggle('open');
+  document.getElementById('hamburger').classList.toggle('active');
+}
+
+document.getElementById('target').addEventListener('click', () => {
+    hamburger();
+  });
+
+let list = document.getElementsByClassName('hamburger-list');
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener('click',() => {
+      hamburger();
+    });
+}
+
+console.log(i)
