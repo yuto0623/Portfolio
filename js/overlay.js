cc6a073d1b5363935008a24b9ecf71f41843b23e
yuto0623 <67817556+yuto0623@users.noreply.github.com>
2020-12-05 21:28:03 +0900

    9846

---
+++ js/overlay.js
@@ -0,0 +1,8 @@
+$(function() {
+  $(".works_box_01").click(function() {
+        $(".overlay_01").fadeIn();　/*ふわっと表示*/
+});
+  $(".overlay").click(function() {
+        $(".overlay").fadeOut();　/*ふわっと消える*/
+});
+});
