fe229cb85cb8c55156c3df0482b991d5e7a7d5ec
yuto0623 <67817556+yuto0623@users.noreply.github.com>
2020-12-06 19:04:02 +0900

    45781

--- js/button.js
+++ js/button.js
@@ -1,19 +1,22 @@
-$(function() {
-  var topBtn = $('#page-top');
+$(function () {
+  var topBtn = $("#page-top");
   topBtn.hide();
   //スクロールが600に達したらボタン表示
   $(window).scroll(function () {
-      if ($(this).scrollTop() > 600) {
-          topBtn.fadeIn();
-      } else {
-          topBtn.fadeOut();
-      }
+    if ($(this).scrollTop() > 600) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
   });
   //スクロールしてトップ
   topBtn.click(function () {
-      $('body,html').animate({
-          scrollTop: 0
-      }, 500);
-      return false;
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
   });
 });
--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,16 @@
 function hamburger() {
-  document.getElementById('hamburger-content').classList.toggle('open');
-  document.getElementById('hamburger').classList.toggle('active');
+  document.getElementById("hamburger-content").classList.toggle("open");
+  document.getElementById("hamburger").classList.toggle("active");
 }
 
-document.getElementById('target').addEventListener('click', () => {
-    hamburger();
-  });
+document.getElementById("target").addEventListener("click", () => {
+  hamburger();
+});
 
-let list = document.getElementsByClassName('hamburger-list');
+let list = document.getElementsByClassName("hamburger-list");
 
 for (let i = 0; i < list.length; i++) {
-  list[i].addEventListener('click',() => {
-      hamburger();
-    });
+  list[i].addEventListener("click", () => {
+    hamburger();
+  });
 }
