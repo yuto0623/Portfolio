5149dea062fecf90aeaeace7c11cc4d46260a67a
yuto0623 <67817556+yuto0623@users.noreply.github.com>
2021-07-27 15:57:33 +0900

    Add files via upload

---
+++ js/overlay.js
@@ -0,0 +1,23 @@
+$(function () {
+  $(".works_box_01").click(function () {
+    $(".overlay_01").fadeIn(); /*ふわっと表示*/
+  });
+  $(".overlay").click(function () {
+    $(".overlay").fadeOut(); /*ふわっと消える*/
+  });
+  $(".works_box_02").click(function () {
+    $(".overlay_02").fadeIn(); /*ふわっと表示*/
+  });
+  $(".works_box_03").click(function () {
+    $(".overlay_03").fadeIn(); /*ふわっと表示*/
+  });
+  $(".works_box_04").click(function () {
+    $(".overlay_04").fadeIn(); /*ふわっと表示*/
+  });
+  $(".works_box_05").click(function () {
+    $(".overlay_05").fadeIn(); /*ふわっと表示*/
+  });
+  $(".works_box_06").click(function () {
+    $(".overlay_06").fadeIn(); /*ふわっと表示*/
+  });
+});
